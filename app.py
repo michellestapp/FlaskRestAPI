4c9e97d4c8131726f5ca2c890226d90ef196cf26
michellestapp <mstapp@rocketmail.com>
2023-04-21 12:36:02 -0500

    Checking for error in CarRes id

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
 from os import environ
+from marshmallow import post_load
 
 load_dotenv()
 
@@ -23,15 +24,48 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String(255), nullable = False)
+    model = db.Column(db.String(255), nullable = False)
+    year = db.Column(db.Integer)    
 
-
+    def __repr__(self):
+        return f'{self.year} {self.make} {self.model}'
 
 # Schemas
+class CarSchema(ma.Schema):
+    @post_load
+    def create(self, data, **kwargs):
+        return Car(**data)
+    
+    class Meta:
+        fields = ("id","make","model","year")
 
+car_schema = CarSchema()
+cars_schema = CarSchema(many = True)
 
 
 # Resources
+class CarListResources(Resource):
+    def get(self):
+        all_cars = Car.query.all()
+        return cars_schema.dump(all_cars)
+    
+    def post(self):
+        data = request.get_json()
+        new_car = car_schema.load(data)
+        db.session.add(new_car)
+        db.session.commit()
+        return car_schema.dump(new_car)
+    
 
+class CarResource(Resource):
+    def get(self,car_id):
+        print(car_id)
+        pass
 
 
 # Routes
+api.add_resource(CarListResources,'/api/cars')
+api.add_resource(CarResource, '/api/cars/<int:car_id>')
